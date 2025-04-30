6ff941a426bef793211f434752c6efef956d1bd3
robinsoj <robinsoj1228@gmail.com>
2025-04-29 20:02:43 -0700

    Working on Mohave county.

--- window.py
+++ window.py
@@ -94,7 +94,7 @@ def main():
         if match:
             number = int(match.group())
             #if number >= 27 and number <= 33:
-            if number in (5, 8):
+            if number in (9, 13):
                 for pt in v.boundaries.points:
                     print(f"{k},{pt.x},-{pt.y}")
                 win.register_drawable(v.boundaries)
